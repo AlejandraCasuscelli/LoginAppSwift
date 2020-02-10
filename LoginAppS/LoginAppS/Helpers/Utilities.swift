0ab93666191fab4d2b2a8526c06a90a7895edb76
AlejandraCasuscelli <31020457+AlejandraCasuscelli@users.noreply.github.com>
2020-02-10 06:42:08 -0800

    Agrego proyecto inicial

---
+++ LoginAppS/LoginAppS/Helpers/Utilities.swift
@@ -0,0 +1,27 @@
+//
+//  Utilities.swift
+//  LoginAppS
+//
+//  Created by Alejandra Casuscelli on 10/02/2020.
+//  Copyright © 2020 Alejandra Casuscelli. All rights reserved.
+//
+
+import UIKit
+
+class Utilities {
+
+    static func styleTextField(_ textfield:UITextField){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
