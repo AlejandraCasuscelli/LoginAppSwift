548d1bd25300a7055791c9a89ddb7a69852ae2fc
Alejandra Casuscelli <>
2020-02-10 08:42:36 -0800

    Agrego firebase

--- LoginAppS/LoginAppS/ViewController/SignUpViewController.swift
+++ LoginAppS/LoginAppS/ViewController/SignUpViewController.swift
@@ -38,15 +38,12 @@ class SignUpViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func validateFields() -> String? {
+        
+        
+        
+        return nil
     }
-    */
     
     @IBAction func signUpTapped(_ sender: Any) {
     }
