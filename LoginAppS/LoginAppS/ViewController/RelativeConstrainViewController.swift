78f3067a8a3e040f5b83985896c19bf2d50d44df
Alejandra Casuscelli <>
2020-02-12 04:16:22 -0800

    Agrego view de practica con relativeconstrain

---
+++ LoginAppS/LoginAppS/ViewController/RelativeConstrainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RelativeConstrainViewController.swift
+//  LoginAppS
+//
+//  Created by Alejandra Casuscelli on 12/02/2020.
+//  Copyright © 2020 Alejandra Casuscelli. All rights reserved.
+//
+
+import UIKit
+
+class RelativeConstrainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
