5331a1ee7a7cab027be2ec3fb0422d91c46fece3
Alejandra Casuscelli <>
2020-02-12 07:29:05 -0800

    Pruebas para conectar a provincia

---
+++ LoginAppS/LoginAppS/ViewController/LoginProvinciaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginProvinciaViewController.swift
+//  LoginAppS
+//
+//  Created by Alejandra Casuscelli on 12/02/2020.
+//  Copyright © 2020 Alejandra Casuscelli. All rights reserved.
+//
+
+import UIKit
+
+class LoginProvinciaViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
